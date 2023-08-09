22cf716fd04fc6ff5a0de6324ff1df5818b9c0bd
RussWong <37031190+RussWong@users.noreply.github.com>
2023-08-09 08:55:14 +0800

    Update 9_gelu.cu

--- 9_gelu.cu
+++ 9_gelu.cu
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <cuda.h>
 #include <cuda_fp16.h>
 #include "cuda_runtime.h"
@@ -17,71 +18,76 @@ __device__ float TanhApprox(float x) {
 }
 
 template<typename T>
-struct FusedFastGeluFunctor {
+struct FusedGeluFunctor {
   static constexpr T alpha = static_cast<T>(0.7978845608028654);
   static constexpr T beta = static_cast<T>(0.044714998453855515);
 
-  __device__ FusedFastGeluFunctor() {};
+  __device__ FusedGeluFunctor() {};
 
-  __device__ T operator()(T x, T m) const {
+  __device__ T operator()(T x) const {
     const T half = static_cast<T>(0.5);
     const T one = static_cast<T>(1);
     const T tanh_in = alpha * (x + beta * x * x * x);
-    return half * x * (one + tanh(tanh_in)) * m;
+    return half * x * (one + tanh(tanh_in));
   }
 };
 
 template<>
-struct FusedFastGeluFunctor<half> {
-  static constexpr float alpha = FusedFastGeluFunctor<float>::alpha;
-  static constexpr float beta = FusedFastGeluFunctor<float>::beta;
-  FusedFastGeluFunctor<float> float_functor;
+struct FusedGeluFunctor<half> {
+  static constexpr float alpha = FusedGeluFunctor<float>::alpha;
+  static constexpr float beta = FusedGeluFunctor<float>::beta;
+  FusedGeluFunctor<float> float_functor;
 
-  __device__ FusedFastGeluFunctor() {};
+  __device__ FusedGeluFunctor() {};
 
   __device__ half operator()(const half x) const {
-    const float tanh_in =
-        __half2float(__float2half_rn(alpha) * (x + __float2half_rn(beta) * x * x * x));
-    const float tanh_out = TanhApprox(tanh_in);
-    return __float2half_rn(0.5F) * x * (__float2half_rn(1.0F) + __float2half_rn(tanh_out));
+    //const float tanh_in =
+    //    __half2float(__float2half_rn(alpha) * (x + __float2half_rn(beta) * x * x * x));
+    //const float tanh_out = TanhApprox(tanh_in);
+    //return __float2half_rn(0.5f) * x * (__float2half_rn(1.0f) + __float2half_rn(tanh_out));
     return static_cast<half>(float_functor(static_cast<float>(x)));
   }
 
-  __device__ void Apply2(half* y, const half* x) const {
-    const half2 x2 = *(reinterpret_cast<const half2*>(x));
-    const float2 tanh_in = __half22float2(
-        __hmul2(__float2half2_rn(alpha),
-                __hadd2(x2, __hmul2(__hmul2(__hmul2(__float2half2_rn(beta), x2), x2), x2))));
-    float2 tanh_out;
-    tanh_out.x = TanhApprox(tanh_in.x);
-    tanh_out.y = TanhApprox(tanh_in.y);
-    const half2 y2 = __hmul2(__hmul2(__hmul2(__float2half2_rn(0.5F), x2),
-                                     __hadd2(__float2half2_rn(1.0F), __float22half2_rn(tanh_out))));
-    *reinterpret_cast<half2*>(y) = y2;
-  }
+  //__device__ void Apply2(half* y, const half* x) const {
+    //const half2 x2 = *(reinterpret_cast<const half2*>(x));
+    //const float2 tanh_in = __half22float2(
+     //   __hmul2(__float2half2_rn(alpha),
+      //          __hadd2(x2, __hmul2(__hmul2(__hmul2(__float2half2_rn(beta), x2), x2), x2))));
+    //float2 tanh_out;
+    //tanh_out.x = TanhApprox(tanh_in.x);
+    //tanh_out.y = TanhApprox(tanh_in.y);
+    //const half2 y2 = __hmul2(__hmul2(__hmul2(__float2half2_rn(0.5F), x2),
+    //                                 __hadd2(__float2half2_rn(1.0F), __float22half2_rn(tanh_out))));
+    //*reinterpret_cast<half2*>(y) = y2;
+  //}
 };
 
 
-template <int VecSize, bool FastMode>
-__global__ void FP16FastGeluFwdCUDAKernel(const __half* x,
+template <int VecSize>
+__global__ void FP16GeluFwdCUDAKernel(const __half* x,
                                                  __half* y,
-                                                 size_t n) {
-  size_t offset =
-      static_cast<size_t>(threadIdx.x + blockIdx.x * blockDim.x) * VecSize;
-  size_t stride = static_cast<size_t>(blockDim.x * gridDim.x) * VecSize;
-  FusedFastGeluFunctor<half> gelu_fwd;
+                                                 int n) {
+  int offset =
+      static_cast<int>(threadIdx.x + blockIdx.x * blockDim.x) * VecSize;
+  int stride = static_cast<int>(blockDim.x * gridDim.x) * VecSize;
+  FusedGeluFunctor<half> gelu_fwd;
+  __half y_reg[VecSize];
   for (; offset < n; offset += stride) {
     using ArrT = AlignedVector<__half, VecSize>;
-    ArrT* in_arr = reinterpret_cast<const ArrT*>(x + offset);
+    const ArrT* in_arr = reinterpret_cast<const ArrT*>(x + offset);
     // ArrT* out_arr = reinterpret_cast<const ArrT*>(y + offset);
-    __half* in = reinterpret_cast<const __half*>(in_arr);
+    const __half* in = reinterpret_cast<const __half*>(in_arr);
     // __half* out = reinterpret_cast<const __half*>(out_arr);
-#pragma unroll
-    for (int i = 0; i < VecSize; i+=2) {
-      // gelu_fwd.apply2(out[i], in[i]);
-      gelu_fwd.apply2(y + offset, in[i]);
+
+    if (VecSize == 1){
+        y_reg[0] = gelu_fwd(in[0]);
+    } else {
+        for (int i = 0; i < VecSize; i+=2) {
+      //gelu_fwd.apply2(y + offset, in[i]);
+            y_reg[i] = gelu_fwd(in[i]);
+        }
     }
-    // reinterpret_cast<const ArrT*>(y + offset) = *reinterpret_cast<const ArrT*>(out);
+    *reinterpret_cast<ArrT*>(y + offset) = *reinterpret_cast<ArrT*>(y_reg);
   }
 }
 
@@ -90,11 +96,11 @@ int main() {
     
     __half *x = new __half[n];
     __half *y = new __half[n];
-    for (size_t i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
       x[i] = (__half)(i);
-      }
-      __half * d_x, *d_y;
+    }
+    __half * d_x, *d_y;
     cudaMalloc((void **)&d_x, n * sizeof(__half));
     cudaMalloc((void **)&d_y, n * sizeof(__half));
     cudaMemcpy(d_x, x, sizeof(__half) * n, cudaMemcpyHostToDevice);
@@ -103,24 +109,26 @@ int main() {
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, 0);
 
-    auto is_aligned = [](const void* p, size_t alignment) {
+    auto is_aligned = [](const void* p, int alignment) {
         return reinterpret_cast<uintptr_t>(p) % alignment == 0;
     };
                                                                       
     constexpr auto kAlignment = alignof(AlignedVector<__half, 8>);                      
     if (n % 8 == 0 && is_aligned(x, kAlignment) && is_aligned(y, kAlignment)) {                                          
-      size_t thread = std::min<size_t>(512, deviceProp.GetMaxThreadsPerBlock()); 
-      size_t block = (n / 8 + thread - 1) / thread;                  
-      block = std::min<size_t>(block, deviceProp.GetCUDAMaxGridDimSize()[0]);                                  
-      FP16FastGeluFwdCUDAKernel<8, true><<<block, thread>>>(x, y, n);                  
+      int thread = std::min<int>(512, deviceProp.maxThreadsPerBlock); 
+      //int block = (n / 8 + thread - 1) / thread;                  
+      //block = std::min<int>(block, deviceProp.maxGridSize[0]);                                  
+      //FP16GeluFwdCUDAKernel<8, true><<<block, thread>>>(x, y, n);  
+      int block = (n + thread - 1) / thread;                  
+      block = std::min<int>(block, deviceProp.maxGridSize[0]);                                  
+      FP16GeluFwdCUDAKernel<1><<<block, thread>>>(d_x, d_y, n);                      
       cudaMemcpy(y, d_y, sizeof(__half) * n, cudaMemcpyDeviceToHost);                                                          
     }   
-
+    printf("pass");
     delete x;
     x = nullptr;
     delete y;
     y = nullptr;
     cudaFree(d_x);
     cudaFree(d_y);
-                                                                   
 }
