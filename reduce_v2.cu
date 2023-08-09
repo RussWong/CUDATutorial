d237e19fee11d667ca348692f064c96f2c7b5823
RussWong <37031190+RussWong@users.noreply.github.com>
2023-08-09 20:45:53 +0800

    Update reduce_v2.cu

--- reduce_v2.cu
+++ reduce_v2.cu
@@ -5,26 +5,26 @@
 // latency: 2.300ms
 template<int blockSize>
 __global__ void reduce_v2(float *d_in,float *d_out){
-    __shared__ float sdata[blockSize];
+    __shared__ float smem[blockSize];
 
     unsigned int tid = threadIdx.x;
     //unsigned int gtid = blockIdx.x * blockSize + threadIdx.x;
     // load: 每个线程加载一个元素到shared mem对应位置
-    sdata[tid] = d_in[tid];
+    smem[tid] = d_in[tid];
     __syncthreads();
 
     // compute: reduce in shared mem
     // 思考这里是如何并行的
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-        if (tid < s) {
-            sdata[tid] += sdata[tid + s];
+    for (unsigned int index = blockDim.x / 2; index > 0; index >>= 1) {
+        if (tid < index) {
+            smem[tid] += smem[tid + index];
         }
         __syncthreads();
     }
 
     // store: write back to global mem
     if (tid == 0) {
-        d_out[blockIdx.x] = sdata[0];
+        d_out[blockIdx.x] = smem[0];
     }
 }
 
