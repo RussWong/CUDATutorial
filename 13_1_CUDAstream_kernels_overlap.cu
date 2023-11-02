c29d9fc87a3216cb3068da74eb5f7bcc9b59710b
RussWong <37031190+RussWong@users.noreply.github.com>
2023-11-02 22:15:46 +0800

    fix codestyle

--- 13_1_CUDAstream_kernels_overlap.cu
+++ 13_1_CUDAstream_kernels_overlap.cu
@@ -4,7 +4,7 @@
 
 __global__ void float_add_one(float* buffer, uint32_t n)
 {
-    int gid = blockDim.x * blockIdx.x + threadIdx.x};
+    int gid = blockDim.x * blockIdx.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
 
     for (int i = gid; i < n; i += stride)
@@ -21,13 +21,13 @@ void float_add_one_launcher(float* buffer, uint32_t n, dim3& threads_per_block,
 
 int main(int argc, char** argv)
 {
-    size_t const buffer_size{1024 * 10240};
-    size_t const num_streams{5};
+    const size_t buffer_size = 1024 * 10240;
+    const size_t num_streams = 5;
 
-    dim3 threads_per_block{1024};
+    dim3 threads_per_block(1024);
     // Try different values for blocks_per_grid and see stream result on nsight.
     // 1, 2, 4, 8, 16, 32, 1024
-    dim3 blocks_per_grid{32};
+    dim3 blocks_per_grid(32);
     
     std::vector<float*> d_buffers(num_streams);
     // below can be replaced by cudaStream_t*;
@@ -41,7 +41,7 @@ int main(int argc, char** argv)
 
     for (auto& stream : streams)
     {
-        cudaStreamCreate(&stream));
+        cudaStreamCreate(&stream);
     }
     // each independent kernel running on each streams to parallize
     for (int i = 0; i < num_streams; ++i)
@@ -52,7 +52,7 @@ int main(int argc, char** argv)
     // host wait each stream to sync
     for (auto& stream : streams)
     {
-        cudaStreamSynchronize(stream));
+        cudaStreamSynchronize(stream);
     }
 
     for (auto& d_buffer : d_buffers)
