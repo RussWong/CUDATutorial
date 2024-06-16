71ef95a46e04cbedc191f3788e900da8c77a4812
zhaowenxin <xingezwx@gmail.com>
2024-06-16 08:44:38 +0800

    fix 10_fused_bias_mask_scale_and_add_fp32

--- 10_fused_bias_mask_scale_and_add/10_fused_bias_mask_scale_and_add_fp32.cu
+++ 10_fused_bias_mask_scale_and_add/10_fused_bias_mask_scale_and_add_fp32.cu
@@ -1,79 +1,174 @@
-#include <stdio.h>
 #include <cuda.h>
+#include <bits/stdc++.h>
 #include "cuda_runtime.h"
-// 注意: 本kernel注释见fp16版本的fused bias mask scale and add
+
+// 实现fp32的fused biasadd mask scale and add的融合算子
+// biasadd + mask + scale + elemwise_add四个算子的融合
+// （x + bias） * mask * scale + addend;
+
+constexpr int n = 100000;
+constexpr int bais_size = 10;
+
 template<typename T>
-struct MaskScaleAndElementwiseAddFunctor {
-  MaskScaleAndElementwiseAddFunctor(const uint8_t* mask, const T* add_val, float scale)
-      : mask(mask), add_val(add_val), scale(scale) {}
-  __device__ T Compute(T x, int64_t i) const {
-    return x * static_cast<T>(static_cast<bool>(mask[i]) * scale) + add_val[i];
-  }
-  const uint8_t* mask;
-  const T* add_val;
-  float scale;
+struct MaskScaleAndElemwiseAddFunctor
+{
+    // 有参构造函数
+    MaskScaleAndElemwiseAddFunctor(const uint8_t * mask, const T * add_val, float scale)
+    :_mask(mask), _add_val(add_val), _scale(scale)
+    {}
+
+    // 重载运算符（）
+    __device__ T operator()(T x, int i) const
+    {
+        return x * static_cast<T>(static_cast<bool>(_mask[i]) * _scale) + _add_val[i];
+    }
+
+    const uint8_t * _mask;
+    const T * _add_val;
+    float _scale;
 };
 
 template<typename FUNCTOR, typename T>
-__global__ void FusedBiasAddCUDAKernelFloat(FUNCTOR functor, const int elem_cnt, const int bias_size,
-                                const T* x, const T* bias, T* y) {
-  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < elem_cnt;
-       i += blockDim.x * gridDim.x){
-    T x_i = x[i] + bias[i % bias_size];
-    y[i] = functor.Compute(x_i, i);
-  }
+__global__ void FusedBaisAdd(FUNCTOR functor, T * dx, T * dy, T * d_bias)
+{
+    int gid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    for (int i = gid; i < n; i += gridDim.x * blockDim.x)
+    {
+        T tmp = dx[i] + d_bias[i % bais_size];
+        dy[i] = functor(tmp, i);
+    }
 }
 
-int main(){
-    constexpr int ele_cnt = 1000;
+// 使用向量化进行存取
+template<typename FUNCTOR, typename T>
+__global__ void FusedBaisAddVecSmem(FUNCTOR functor, T * dx, T * dy, T * d_bias)
+{
+    int gid = blockDim.x * blockIdx.x + threadIdx.x;
+    int tid = threadIdx.x;
+
+    __shared__ T smem[bais_size];
+
+    // 将d_bias放在shared memory上
+    if (tid < bais_size)
+        smem[tid] = d_bias[tid];
+    __syncthreads();
+
+    for (int i = gid; i < n / 4; i += gridDim.x * blockDim.x)
+    {
+        float4 a = reinterpret_cast<float4 *>(dx)[i];
+        float4 b;
+
+        b.x = functor(a.x + smem[(i * 4) % bais_size], i * 4);
+        b.y = functor(a.y + smem[(i * 4 + 1) % bais_size], i * 4 + 1);
+        b.z = functor(a.z + smem[(i * 4 + 2) % bais_size], i * 4 + 2);
+        b.w = functor(a.w + smem[(i * 4 + 3) % bais_size], i * 4 + 3);
+
+        reinterpret_cast<float4*>(dy)[i] = b;
+    }
+}
+
+bool CheckRight(float * y, float * groudTruth)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        if (y[i] != groudTruth[i])
+        {
+            printf("y[%d] : %f \n", i, y[i]);
+            printf("groundTruth[%d] : %f\n", i, groudTruth[i]);
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
     float scale = 0.5;
-    uint8_t* mask_tensor = new uint8_t[ele_cnt];
-    float* add_val = new float[ele_cnt];
-    for (int i = 0; i < ele_cnt; i++){
+    uint8_t * mask_tensor = new uint8_t[n];
+    float * add_val = new float[n];
+    // 初始化
+    for (int i = 0; i < n; ++i)
+    {
         mask_tensor[i] = (uint8_t)(i);
         add_val[i] = (float)(i);
     }
-    int bias_size = 10;
-    float *x = (float*) malloc(sizeof(float) * ele_cnt);
-    float *y = (float*) malloc(sizeof(float) * ele_cnt);
-    float *bias = (float*) malloc(sizeof(float) * bias_size);
-    for (int i = 0; i < ele_cnt; i++)
+
+    float * x = (float *)malloc(sizeof(float) * n);
+    float * y = (float *)malloc(sizeof(float) * n);
+    float * bias = (float *)malloc(sizeof(float) * bais_size);
+    for (int i = 0; i < n; ++i)
     {
-      x[i] = (float)(i);
+        x[i] = (float)(i);
+        y[i] = 0.0f;
     }
-    float *d_x, *d_y, *d_bias;
-    cudaMalloc((void **)&d_x, ele_cnt * sizeof(float));
-    cudaMalloc((void **)&d_y, ele_cnt * sizeof(float));
-    cudaMalloc((void **)&d_bias, bias_size * sizeof(float));
-    cudaMemcpy(d_x, x, sizeof(float) * ele_cnt, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, y, sizeof(float) * ele_cnt, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_bias, bias, sizeof(float) * bias_size, cudaMemcpyHostToDevice);
-    uint8_t *d_mask_tensor;
-    float *d_add_val;
-    cudaMalloc((void **)&d_mask_tensor, ele_cnt * sizeof(uint8_t));
-    cudaMalloc((void **)&d_add_val, ele_cnt * sizeof(float));
-    cudaMemcpy(d_mask_tensor, mask_tensor, sizeof(float) * ele_cnt, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_add_val, add_val, sizeof(float) * ele_cnt, cudaMemcpyHostToDevice);
+    for (int i = 0; i < bais_size; ++i)
+        bias[i] = i;
+
+    float * groudTruth = (float *)malloc(sizeof(float) * n);
+    for (int i = 0; i < n; ++i)
+    {
+        groudTruth[i] = (x[i] + bias[i % bais_size]) * static_cast<float>(static_cast<bool>(mask_tensor[i]) * scale) + add_val[i];
+    }
+
+    float * dx, * dy, * d_bias;
+    cudaMalloc((void **)&dx, sizeof(float) * n);
+    cudaMalloc((void **)&dy, sizeof(float) * n);
+    cudaMalloc((void **)&d_bias, sizeof(float) * bais_size);
+    cudaMemcpy(dx, x, sizeof(float) * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(dy, y, sizeof(float) * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_bias, bias, sizeof(float) * bais_size, cudaMemcpyHostToDevice);
+    uint8_t * d_mask_tensor;
+    float * d_add_val;
+    cudaMalloc((void **)&d_mask_tensor, sizeof(uint8_t) * n);
+    cudaMalloc((void **)&d_add_val, sizeof(float) * n);
+    cudaMemcpy(d_mask_tensor, mask_tensor, sizeof(uint8_t) * n, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_add_val, add_val, sizeof(float) * n, cudaMemcpyHostToDevice);
 
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, 0);
-    int maxblocks = deviceProp.maxGridSize[0];
-    int blockSize = 256;
-    int gridSize = std::min((ele_cnt + blockSize - 1) / blockSize, maxblocks);
-    MaskScaleAndElementwiseAddFunctor<float> mask_scale_and_elementwise_add_func(mask_tensor, add_val, scale);
-    FusedBiasAddCUDAKernelFloat<<<gridSize , blockSize>>>(mask_scale_and_elementwise_add_func, ele_cnt, bias_size, d_x, d_bias, d_y);
-    cudaMemcpy(y, d_y, sizeof(float) * ele_cnt, cudaMemcpyDeviceToHost);
+    int blockSize = 512;
+    int gridSize = std::min((n + blockSize - 1) / blockSize, deviceProp.maxGridSize[0]);
 
+    MaskScaleAndElemwiseAddFunctor<float> functor(d_mask_tensor, d_add_val, scale);
+
+    dim3 Block(blockSize);
+    dim3 Grid(gridSize);
+
+    float milliseconds = 0.0f;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+    
+    for (int i = 0; i < 1000; ++i)
+        FusedBaisAdd<<<Grid, Block>>>(functor, dx, dy, d_bias);
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    cudaMemcpy(y, dy, sizeof(float) * n, cudaMemcpyDeviceToHost);
+
+    bool isRight = CheckRight(y, groudTruth);
+    if (isRight)
+        printf("结果正确\n");
+    else
+        printf("结果错误\n");    
+
+    printf("it costs %f s \n", milliseconds/1000);
+
+    cudaFree(dx);
+    cudaFree(dy);
+    cudaFree(d_bias);
+    cudaFree(d_add_val);
+    cudaFree(d_mask_tensor);
     free(x);
     free(y);
     free(bias);
+    free(groudTruth);
+    delete mask_tensor;
+    mask_tensor = nullptr;
     delete add_val;
     add_val = nullptr;
-    delete mask_tensor;
-    add_val = nullptr;
-    cudaFree(d_x);
-    cudaFree(d_y);
-    cudaFree(d_bias);
-    cudaFree(d_mask_tensor);
-    cudaFree(d_add_val);
 }
